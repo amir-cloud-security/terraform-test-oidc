f504d221378f8892b6967515f09e57f5dff2c390
amir-cloud-security <211122196+amir-cloud-security@users.noreply.github.com>
2025-05-14 20:02:37 +0100

    Refactor Terraform configuration: restore backend and provider files

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-provider "aws" {
-  region = "eu-west-1"
-}
 
-terraform {
-  backend "s3" {
-    bucket         = "test-bucket-35678989864"
-    key            = "terraform/state"
-    region         = "eu-west-1"
-    encrypt        = true
-  }
-}
 
 resource "aws_s3_bucket" "test-bucket121314214" {
   bucket = "example-terraform-bucket"
