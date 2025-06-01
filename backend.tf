7e273018fe8a0d64c92faa8c6005186734e783d4
Amir Cloud Security <211122196+amir-cloud-security@users.noreply.github.com>
2025-06-01 20:55:02 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-bucket-35678989864"
+    bucket         = "my-secure-tf-state-235423434"
     key            = "terraform/state"
     region         = "eu-west-1"
     encrypt        = true
